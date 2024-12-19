0179bb7ad92c0e2ee6748bf59f3e86123b7d49fe
Gavin Heverly-Coulson <gavin.heverlycoulson@abcellera.com>
2024-12-18 17:02:14 -0800

    day 8 part 2

--- day08.py
+++ day08.py
@@ -14,6 +14,9 @@ class Antenna(XYCoord):
         """
         return self + (self - other)
 
+    def as_xycoord(self) -> XYCoord:
+        return XYCoord(self.x, self.y)
+
 
 class AntennaMap(BoundedGrid):
     def __init__(self, path: str):
@@ -29,7 +32,8 @@ class AntennaMap(BoundedGrid):
 
     def find_antinodes(self) -> int:
         """
-        Find all antinodes on the map, return the number existing
+        Find all antinodes on the map
+        Return the number of unique antinode positions
         """
         all_antis = set()
         for freq, antennae in self.antennae.items():
@@ -43,6 +47,37 @@ class AntennaMap(BoundedGrid):
 
         return len(all_antis)
 
+    def find_resonant_antinodes(self) -> int:
+        """
+        Find all antinodes on the map, taking into account resonant harmonics
+        Resonant harmonics means that there are antinodes at every position along the
+        line created by two antennae, evenly spaced by the distance between the two antennae
+
+        Return the number of unique antinode positions
+        """
+        all_antis = set()
+
+        for freq, antennae in self.antennae.items():
+            for a, b in combinations(antennae, 2):
+                # The positions of the two antennae are themselves antinodes
+                all_antis.update([a.as_xycoord(), b.as_xycoord()])
+
+                # Find all antinodes starting from a and working away from b
+                dist = a - b
+                antinode = a + dist
+                while self._within_bounds(antinode):
+                    all_antis.add(antinode)
+                    antinode = antinode + dist
+
+                # Find all antinodes starting from b and working away from a
+                dist = b - a
+                antinode = b + dist
+                while self._within_bounds(antinode):
+                    all_antis.add(antinode)
+                    antinode = antinode + dist
+
+        return len(all_antis)
+
 
 if __name__ == "__main__":
     path = "inputs/day08"
@@ -50,3 +85,6 @@ if __name__ == "__main__":
 
     all_found = map.find_antinodes()
     print("Number of identified antinodes:", all_found)
+
+    found = map.find_resonant_antinodes()
+    print("Number of resonant antinodes identified:", found)
